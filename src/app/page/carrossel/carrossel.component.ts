4f33d1cfc2c7cb797d7ebdc92a05b856bb12fc87
euquias <willmagalhaes25@gmail.com>
2022-10-14 01:43:18 -0300

    alterando carrosel

--- src/app/page/carrossel/carrossel.component.ts
+++ src/app/page/carrossel/carrossel.component.ts
@@ -1,20 +1,17 @@
-import { Component,  OnInit,   } from '@angular/core';
-
-
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carrossel',
   templateUrl: './carrossel.component.html',
   styleUrls: ['./carrossel.component.css'],
- 
+
 })
 export class CarrosselComponent implements OnInit {
 
   constructor() { }
- 
+
   ngOnInit(): void {
   }
-  
+
 }
 
